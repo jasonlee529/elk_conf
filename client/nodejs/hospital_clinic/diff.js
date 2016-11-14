a4928d7bc251e220330ef9ebfa724227eb571e01
jason <libo529@gmail.com>
2016-11-14 18:38:34 +0800

    回调查询es

--- client/nodejs/hospital_clinic/diff.js
+++ client/nodejs/hospital_clinic/diff.js
@@ -3,28 +3,54 @@
  */
 var es = require('elasticsearch');
 var mysql = require('mysql');
-/*
-var client199 = es.client({
-  host: "192.168.1.199:9230",
-  log: "trace"}
+
+var attrs=[];
+
+var connection = mysql.createConnection({
+  host:'192.168.1.199',
+  user:'web',
+  password:'web_123qaz',
+  database:'web_clinic'
+});
+
+connection.connect();
+connection.query('select * from infisa_extract_attribute',function(err,rows,fields){
+  rows.forEach(function(n){
+    esResult(n);
+  })
+})
+connection.end();
+console.log("长度"+attrs.length);
+
+var client199 = es.Client({
+  host: "192.168.1.91:9200",
+  log: "info"}
 );
+/*
 var client96 = es.client({
   host: "192.168.1.95:9200",
   log: "trace"
 });
 */
+function esResult(n){
+  client199.search({
+   index:'hospital_clinic',
+   body:{
+     query:{
+       match_all:{}
+     },
+     size:0,
+     aggs:{
+       agg_name:{
+         terms:{field:n.es_field}
+       }
+     }
+   }
+  },function(error,response){
+    var aggs = response.aggregations.agg_name.buckets;
+    console.log(n.es_field);
+    console.log(JSON.stringify(aggs));
 
-var connection = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'sa',
-  database:'mysql'
-});
+  });
+};
 
-connection.connect();
-connection.query('select * from user',function(err,rows,fields){
-  rows.forEach(function(n){
-      console.log(n);
-  })
-})
-connection.end();
