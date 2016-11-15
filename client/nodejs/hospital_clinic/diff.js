64b5b4e2d89666bf9e1d7c45c41e10d3d65af8b6
jason <libo529@gmail.com>
2016-11-15 11:39:49 +0800

    修改sql

--- client/nodejs/hospital_clinic/diff.js
+++ client/nodejs/hospital_clinic/diff.js
@@ -16,15 +16,30 @@ var connection = mysql.createConnection({
 connection.connect();
 connection.query('select * from infisa_extract_attribute',function(err,rows,fields){
   rows.forEach(function(n){
-    esResult(n);
+    var aggs =  esResult(n);
+    var data = sqlResult(n);
+    console.log(aggs)
+    aggs.forEach(function(agg,index){
+        console.log(agg) ;
+      console.log(data[index]);
+    })
   })
 })
-connection.end();
+//connection.end();
 
+function sqlResult(n){
+  var lastIndex = n.sql_column.lastIndexOf(".");
+  var tableName = n.sql_column.substring(0,lastIndex);
+  var column = n.sql_column.substring(lastIndex+1);
+  console.log(tableName+"   "+column);
+  connection.query(" select distinct "+column+" , count(1) cnt from "+tableName+" group by "+column+" order by cnt desc limit 20" , function(err,rows,fields){
+    return rows;
+  });
+}
 var client199 = es.Client({
   host: "192.168.1.91:9200",
-  log: "info"}
-);
+  log: "info"
+});
 /*
 var client96 = es.client({
   host: "192.168.1.95:9200",
@@ -49,7 +64,7 @@ function esResult(n){
     var aggs = response.aggregations.agg_name.buckets;
     console.log(n.es_field);
     console.log(JSON.stringify(aggs));
-
+    return aggs;
   });
 };
 
