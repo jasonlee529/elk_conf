a5a6897b6c6550f03055a5290122e82d49e419d5
jason <libo529@gmail.com>
2016-11-18 11:47:48 +0800

    update

---
+++ client/nodejs/hospital_clinic/demos/span_first.js
@@ -0,0 +1,38 @@
+/**
+ * 测试相对位置大小的数据
+ */
+var es = require('elasticsearch'),
+    config = require('../config');
+var client = es.Client(config.es);
+
+client.search({
+    index: "hospital_clinic",
+    body: {
+        "query": {
+            "span_first": {
+                match: {
+                    span_term: {
+                        "oper_info.oper_name": "冠状动脉造影术"
+                    }
+                },
+                end: 10
+            }
+        },
+        "filtered": {
+            "filter": {
+                "script": {
+                    "script": "doc['oper_info.oper_date'].value > doc['drugs_in_hospital.drug_day'].value"
+                }
+            }
+        },
+        "_source": [
+            "oper_info.oper_date",
+            "drugs_in_hospital.drug_day"
+        ],
+        "size": 2
+    },
+    function(err, response) {
+        console.log(err);
+        console.log(response);
+    }
+})
